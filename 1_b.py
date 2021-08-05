ff719bb68270a728baefa941f00448c61877d95f
Joshua Green <joshuagreen424@gmail.com>
2021-08-05 01:12:38 -0700

    Assignment 1_b finished

---
+++ 1_b.py
@@ -0,0 +1,48 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+print(50+50)
+
+
+# %%
+print(100-10)
+
+
+# %%
+print(6**6)
+
+
+# %%
+#30+*6
+
+
+# %%
+6^6
+
+
+# %%
+6+6+6+6+6+6
+
+
+# %%
+print("Hello World")
+
+
+# %%
+print("Hello World : 10")
+
+
+# %%
+import math
+def mortgage(p,r,l):
+    return math.ceil(p*(r/100/12*(1+r/100/12)**l)/((1+r/100/12)**l-1))
+
+
+# %%
+mortgage(800000, 6, 103)
+
+
+# %%
+
+
+
