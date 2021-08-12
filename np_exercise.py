a2a1b4fbe299bb21e3cfe4dd2d0b1ab598b1f390
Joshua Green <joshuagreenss@smoothstack.com>
2021-08-12 14:34:34 -0700

    Np exercises done

---
+++ np_exercise.py
@@ -0,0 +1,66 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import numpy as np
+
+
+# %%
+a1 = np.zeros(10)
+
+
+# %%
+print(a1)
+
+
+# %%
+a2 = np.ones(10)
+
+
+# %%
+print(a2)
+
+
+# %%
+a3 = np.ones(10)*5
+
+
+# %%
+print(a3)
+
+
+# %%
+a4 = np.arange(10,51)
+
+
+# %%
+print(a4)
+
+
+# %%
+a5 = np.arange(10,51,2)
+
+
+# %%
+print(a5)
+
+
+# %%
+a6 = np.arange(0,9).reshape((3,3))
+
+
+# %%
+print(a6)
+
+
+# %%
+a7 = np.eye(3)
+
+
+# %%
+print(a7)
+
+
+# %%
+print(np.random.rand())
+
+
